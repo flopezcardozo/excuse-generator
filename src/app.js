340cea359c35c8d5a3bf6b2e25172b9ab7c0485f
flopezcardozo <flopezcardozo@gmail.com>
2021-08-23 23:21:29 +0000

    ejercicio finalizado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,36 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  console.log(excusa);
+  let excusa = excuseFunc(who, action, what, when);
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = excusa;
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuseFunc(array1, array2, array3, array4) {
+  let whoRandomNumber = Math.floor(Math.random() * array1.length);
+  let actionRandomNumber = Math.floor(Math.random() * array2.length);
+  let whatRandomNumber = Math.floor(Math.random() * array3.length);
+  let whenRandomNumber = Math.floor(Math.random() * array4.length);
+
+  return (
+    array1[whoRandomNumber] +
+    " " +
+    array2[actionRandomNumber] +
+    " " +
+    array3[whatRandomNumber] +
+    " " +
+    array4[whenRandomNumber]
+  );
+}
